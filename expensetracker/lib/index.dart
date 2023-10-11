e00bfeef6994f0ec563cb9b885e7e98f5e201cbd
richajituri1 <richajituri@doshaheen.com>
2023-10-11 20:05:18 +0530

    feat: Expense Tracker Application
    Explain why this change is being made: -
    - Flutter application Expense Tracker which displays all the expenses
      along with categories and date, even allows user to add expenses,
      delete and undo
    
    -

---
+++ expensetracker/lib/index.dart
@@ -0,0 +1,8 @@
+export 'package:expensetracker/main.dart';
+export 'package:expensetracker/models/expense.dart';
+export 'package:expensetracker/widget/expenses.dart';
+export 'package:expensetracker/widget/expenses_list/expense_item.dart';
+export 'package:expensetracker/widget/expenses_list/expenses_list.dart';
+export 'package:expensetracker/widget/new_expense.dart';
+export 'package:expensetracker/widget/chart/chart.dart';
+export 'package:expensetracker/widget/chart/chart_bar.dart';
---
+++ expensetracker/lib/widget/expenses_list/expense_item.dart
@@ -0,0 +1,41 @@
+import 'package:expensetracker/index.dart';
+import 'package:flutter/material.dart';
+
+class ExpenseItem extends StatelessWidget {
+  const ExpenseItem(this.expense, {super.key});
+    
+    final Expense expense;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: 10,
+        vertical: 3,
+      ),
+      child: Card(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(expense.title, style: Theme.of(context).textTheme.titleLarge,),
+            const SizedBox(height: 50,),
+            Row(
+              children: [
+                Text('\$${expense.amount.toStringAsFixed(2)}'), // 12.3433 => 12.34
+                const Spacer(),
+                Row(
+                  children: [
+                    Icon(categoryIcons[expense.category]),
+                    const SizedBox(width: 9,),
+                    Text(expense.formattedDate),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
